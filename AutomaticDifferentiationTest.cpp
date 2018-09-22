ff825354cbd392e56cf5db21c9c51ae4dcfa1df0
Hiroshi Nakamura <43459320+Hiroshi-Nakamura@users.noreply.github.com>
2018-09-22 09:32:57 +0900

    Used eigen-lib for operator() inputs

--- AutomaticDifferentiationTest.cpp
+++ AutomaticDifferentiationTest.cpp
@@ -15,18 +15,25 @@ int main(int argc, char** argv)
             AutomaticDifferentiation::FuncPtr<double,2> c(new AutomaticDifferentiation::Constant<double,2>(3.0));
             AutomaticDifferentiation::FuncPtr<double,2> y=x0*x0+x0*x1+x0/x1+c;
 
+#ifdef WITHOUT_EIGEN
             const std::array<double,2> val{10.0,2.0};
+#else
+            Eigen::Matrix<double,2,1> val{10.0,2.0};
+#endif // WITHOUT_EIGEN
             std::cout << "y(val)=" << (*y)(val) << std::endl;
             AutomaticDifferentiation::FuncPtr<double,2> dy_0=(*y).derivative(0);
             std::cout << "dy_0(val)" << (*dy_0)(val) << std::endl;
             AutomaticDifferentiation::FuncPtr<double,2> dy_0_1=(*dy_0).derivative(1);
             std::cout << "dy_0_1(val)" << (*dy_0_1)(val) << std::endl;
         }
-
         {
             AutomaticDifferentiation::FuncPtr<double,1> x(new AutomaticDifferentiation::Variable<double,1>(0));
             AutomaticDifferentiation::FuncPtr<double,1> y=cos(x);
+#ifdef WITHOUT_EIGEN
             const std::array<double,1> alpha{M_PI/3.0};
+#else
+            const Eigen::Matrix<double,1,1> alpha{M_PI/3.0};
+#endif // WITHOUT_EIGEN
             std::cout << "y(alpha)=" << (*y)(alpha) << std::endl;
             AutomaticDifferentiation::FuncPtr<double,1> dy=(*y).derivative();
             std::cout << "dy(alpha)=" << (*dy)(alpha) << std::endl;
@@ -36,7 +43,11 @@ int main(int argc, char** argv)
             auto y=f(AutomaticDifferentiation::createVariables<double,3>());
             auto jac=AutomaticDifferentiation::jacobian(y);
             auto hes=AutomaticDifferentiation::hessian(y);
+#ifdef WITHOUT_EIGEN
             const std::array<double,3> val{10.0, 2.0, 5.0};
+#else
+            const Eigen::Matrix<double,3,1> val{10.0, 2.0, 5.0};
+#endif // WITHOUT_EIGEN
             std::cout << "y(val)=" << (*y)(val) << std::endl;
 #ifdef WITHOUT_EIGEN
             std::cout << "jac(y)(val)=" << std::endl << AutomaticDifferentiation::to_string<double,3>(jac(val)) << std::endl;
