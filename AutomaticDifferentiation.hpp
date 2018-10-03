e605d6eab034f7ae2d9efa51be262a1d87036fb1
Hiroshi Nakamura <43459320+Hiroshi-Nakamura@users.noreply.github.com>
2018-10-03 15:22:55 +0900

    adding another implementation using std::vector instead of std::array

--- AutomaticDifferentiation.hpp
+++ AutomaticDifferentiation.hpp
@@ -5,10 +5,321 @@
 #include <eigen3/Eigen/Core>
 #endif // WITHOUT_EIGEN
 #include <array>
+#include <vector>
 #include <memory>
 #include <cmath>
 #include <sstream>
 #include <iomanip>
+
+#define OPERATOR_FUNC_DEFINE_VEC(operant,operant_name) \
+    template<typename T> \
+    FuncPtr<T> operator operant (const FuncPtr<T>& left, const FuncPtr<T>& right){ \
+        return FuncPtr<T>(new Operator<T>(FuncType::operant_name, left, right)); \
+    } \
+    template<typename T> \
+    FuncPtr<T> operator operant (const FuncPtr<T>& left, const T& right_val){ \
+        return FuncPtr<T>(new Operator<T>(FuncType::operant_name, left, FuncPtr<T>(new Constant<T>(right_val)))); \
+    } \
+    template<typename T> \
+    FuncPtr<T> operator operant (const T& left_val, const FuncPtr<T>& right){ \
+        return FuncPtr<T>(new Operator<T>(FuncType::operant_name, FuncPtr<T>(new Constant<T>(left_val)), right)); \
+    }
+
+#define MATH_FUNC_DEFINE_VEC(operant,operant_name) \
+    template<typename T> \
+    FuncPtr<T> operant(const FuncPtr<T>& functor){ \
+        return FuncPtr<T>(new Operator<T>(FuncType::operant_name, functor)); \
+    }
+
+
+namespace AutomaticDifferentiation_Vector {
+
+    template<typename T>
+    class Functor {
+    public:
+        virtual T operator()(const std::vector<T>& x) const =0; /// The same usage as a mathmatical function.
+        virtual std::shared_ptr<Functor<T>> derivative(size_t idx=0) const =0; /// Get derivative of this Functor. The return is also the derived class of Functor.
+    };
+
+    template<typename T>
+    using FuncPtr=std::shared_ptr<Functor<T>>;
+
+    template<typename T>
+    class Constant : public Functor<T> {
+    private:
+        const T val;
+    public:
+        Constant(const T& _val) : val(_val) {}
+        virtual T operator()(const std::vector<T>& x) const
+        {
+            return val;
+        }
+        virtual FuncPtr<T> derivative(size_t idx) const
+        {
+            return FuncPtr<T>(new Constant(0.0));
+        }
+    };
+
+    enum FuncType { SUM, DIFFERENCE, PRODUCT, QUOTIENT, MINUS, COS, SIN, TAN, ACOS, ASIN, ATAN, EXP, LOG, SQRT };
+
+    template<typename T>
+    class Operator : public Functor<T> {
+        const FuncType func_type;
+        const FuncPtr<T> left;
+        const FuncPtr<T> right;
+    public:
+        Operator(FuncType _func_type, const FuncPtr<T>& _left, const FuncPtr<T>& _right=nullptr) : func_type(_func_type), left(_left), right(_right) {}
+        virtual T operator()(const std::vector<T>& x) const
+        {
+            switch(func_type){
+            case FuncType::SUM:
+                return (*left)(x) + (*right)(x);
+                break;
+            case FuncType::DIFFERENCE:
+                return (*left)(x) - (*right)(x);
+                break;
+            case FuncType::PRODUCT:
+                return (*left)(x) * (*right)(x);
+                break;
+            case FuncType::QUOTIENT:
+                return (*left)(x) / (*right)(x);
+                break;
+            case FuncType::MINUS:
+                return -(*left)(x);
+                break;
+            case FuncType::COS:
+                return std::cos( (*left)(x) );
+                break;
+            case FuncType::SIN:
+                return std::sin( (*left)(x) );
+                break;
+            case FuncType::TAN:
+                return std::tan( (*left)(x) );
+                break;
+            case FuncType::ACOS:
+                return std::acos( (*left)(x) );
+                break;
+            case FuncType::ASIN:
+                return std::asin( (*left)(x) );
+                break;
+            case FuncType::ATAN:
+                return std::atan( (*left)(x) );
+                break;
+            case FuncType::EXP:
+                return std::exp( (*left)(x) );
+                break;
+            case FuncType::LOG:
+                return std::log( (*left)(x) );
+                break;
+            case FuncType::SQRT:
+                return std::sqrt( (*left)(x) );
+                break;
+            default:
+                throw std::string("Not defined operator in ")+__func__;
+            }
+        }
+        virtual FuncPtr<T> derivative(size_t idx) const
+        {
+            switch(func_type){
+            case FuncType::SUM:
+                return (*left).derivative(idx) + (*right).derivative(idx);
+                break;
+            case FuncType::DIFFERENCE:
+                return (*left).derivative(idx) - (*right).derivative(idx);
+                break;
+            case FuncType::PRODUCT:
+                return (*left).derivative(idx) * right + left * (*right).derivative(idx);
+                break;
+            case FuncType::QUOTIENT:
+                return (*left).derivative(idx) / right - left * (*right).derivative(idx) / right / right;
+                break;
+            case FuncType::COS:
+                return  - (*left).derivative(idx) * sin(left);
+                break;
+            case FuncType::SIN:
+                return (*left).derivative(idx) * cos(left);
+                break;
+            case FuncType::TAN:
+                return (*left).derivative(idx) / cos(left) / cos(left);
+                break;
+            case FuncType::ACOS:
+                return  - (*left).derivative(idx) / sqrt(1.0 - left*left);
+                break;
+            case FuncType::ASIN:
+                return (*left).derivative(idx)  / sqrt(1.0 - left*left);
+                break;
+            case FuncType::ATAN:
+                return (*left).derivative(idx) / (left*left + 1.0);
+                break;
+            case FuncType::EXP:
+                return (*left).derivative(idx) * exp(left);
+                break;
+            case FuncType::LOG:
+                return (*left).derivative(idx) / (left);
+                break;
+            case FuncType::SQRT:
+                return (*left).derivative(idx) / 2.0*sqrt(left);
+                break;
+            default:
+                throw std::string("Not defined operator in ")+__func__;
+            }
+        }
+    };
+
+    template<typename T>
+    class Variable : public Functor<T> {
+    private:
+        const size_t index;
+    public:
+        Variable(const size_t _index) : index(_index) {}
+        virtual T operator()(const std::vector<T>& x) const
+        {
+            return x[index];
+        }
+        FuncPtr<T> derivative(size_t idx) const
+        {
+            if(idx==index) return FuncPtr<T>(new Constant<T>(1.0));
+            else           return FuncPtr<T>(new Constant<T>(0.0));
+        };
+    };
+
+    OPERATOR_FUNC_DEFINE_VEC(+,SUM);
+    OPERATOR_FUNC_DEFINE_VEC(-,DIFFERENCE);
+    OPERATOR_FUNC_DEFINE_VEC(*,PRODUCT);
+    OPERATOR_FUNC_DEFINE_VEC(/,QUOTIENT);
+
+    MATH_FUNC_DEFINE_VEC(cos,COS);
+    MATH_FUNC_DEFINE_VEC(sin,SIN);
+    MATH_FUNC_DEFINE_VEC(tan,TAN);
+    MATH_FUNC_DEFINE_VEC(acos,ACOS);
+    MATH_FUNC_DEFINE_VEC(asin,ASIN);
+    MATH_FUNC_DEFINE_VEC(atan,ATAN);
+    MATH_FUNC_DEFINE_VEC(exp,EXP);
+    MATH_FUNC_DEFINE_VEC(log,LOG);
+    MATH_FUNC_DEFINE_VEC(sqrt,SQRT);
+
+    template<typename T>
+    FuncPtr<T> operator-(const FuncPtr<T>& functor){
+        return FuncPtr<T>(new Operator<T>(FuncType::MINUS, functor));
+    }
+
+    ///
+    /// utulity-- create variables
+    ///
+    template<typename T>
+    std::vector<FuncPtr<T>> createVariables(size_t dim)
+    {
+        std::vector<FuncPtr<T>> rtn;
+        for(size_t i=0; i<dim; i++){
+            rtn.emplace_back(new Variable<T>(i));
+        }
+        return rtn;
+    }
+
+    ///
+    /// utulity-- calculate Jacobian Functor
+    ///
+    template<typename T>
+    class VecFuncPtr {
+    private:
+        std::vector<FuncPtr<T>> func_ptr;
+    public:
+        VecFuncPtr(const std::vector<FuncPtr<T>>& _func_ptr) : func_ptr(_func_ptr) {}
+        virtual std::vector<T> operator()(const std::vector<T>& x) const
+        {
+            std::vector<T> rtn;
+            for(size_t i=0; i<func_ptr.size(); i++){
+                rtn.push_back(std::move((*func_ptr[i])(x)));
+            }
+            return rtn;
+        }
+        virtual FuncPtr<T> operator[](const size_t idx) const
+        {
+            return func_ptr[idx];
+        }
+        virtual std::vector<FuncPtr<T>> to_vector() const
+        {
+            return func_ptr;
+        }
+    };
+
+    template<typename T>
+    VecFuncPtr<T> jacobian(const FuncPtr<T>& f, const size_t dim)
+    {
+        std::vector<FuncPtr<T>> rtn;
+        for(size_t i=0; i<dim; i++){
+            rtn.push_back(move((*f).derivative(i)));
+        }
+        return VecFuncPtr<T>(rtn);
+    }
+
+
+    ///
+    /// utulity-- calculate Hessian Functor
+    ///
+    template<typename T>
+    class MatFuncPtr {
+    private:
+        std::vector<std::vector<FuncPtr<T>>> func_ptr;
+    public:
+        MatFuncPtr(const std::vector<std::vector<FuncPtr<T>>>& _func_ptr) : func_ptr(_func_ptr) {}
+        virtual std::vector<std::vector<T>> operator()(const std::vector<T>& x) const
+        {
+            size_t dim=func_ptr.size();
+            std::vector<std::vector<T>> rtn(dim,std::vector<T>(dim));
+            for(size_t i=0; i<dim; i++){
+                for(size_t j=0; j<dim; j++){
+                    rtn[i][j]=(*func_ptr[i][j])(x);
+                }
+            }
+            return rtn;
+        }
+        virtual std::vector<FuncPtr<T>> operator[](const size_t idx) const
+        {
+            return func_ptr[idx];
+        }
+
+    };
+
+    template<typename T>
+    MatFuncPtr<T> hessian(const FuncPtr<T>& f, const size_t dim)
+    {
+        std::vector<std::vector<FuncPtr<T>>> rtn;
+        auto jac=jacobian(f,dim);
+        for(size_t i=0; i<dim; i++){
+            rtn.push_back(std::move(jacobian(jac[i],dim).to_vector()));
+        }
+        return MatFuncPtr<T>(rtn);
+    }
+
+    ///
+    /// utulity-- convert string for display
+    ///
+    template<typename T>
+    std::string to_string(const std::vector<T>& vec)
+    {
+        std::ostringstream oss;
+        for(auto e: vec){
+            oss << " " << std::setw(8) << e << std::endl;
+        }
+        return oss.str();
+    }
+
+    template<typename T>
+    std::string to_string(const std::vector<std::vector<T>>& mat)
+    {
+        std::ostringstream oss;
+        for(auto row: mat){
+            for(auto e: row){
+                oss << " " << std::setw(8) << e ;
+            }
+            oss << std::endl;
+        }
+        return oss.str();
+    }
+}
+
+
 
 #define OPERATOR_FUNC_DEFINE(operant,operant_name) \
     template<typename T, int DIM> \
@@ -31,7 +342,7 @@
     }
 
 
-namespace AutomaticDifferentiation {
+namespace AutomaticDifferentiation_Array {
 
     template<typename T, int DIM>
     class Functor {
@@ -359,4 +670,9 @@ namespace AutomaticDifferentiation {
     }
 #endif // WITHOUT_EIGEN
 }
+
+/// I recommend "AutomaticDifferentiation_Vector".
+/// AutomaticDifferentiation_Array will be removed.
+namespace AutomaticDifferentiation=AutomaticDifferentiation_Vector;
+
 #endif // AUTOMATICDIFFERENTIATION_HPP_INCLUDED
--- AutomaticDifferentiationTest.cpp
+++ AutomaticDifferentiationTest.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 template<typename T>
-T f(std::array<T,3> x){
+T f(std::vector<T> x){
     return x[0]*x[0]+x[1]*x[2]+1.0;
 }
 
@@ -10,53 +10,41 @@ int main(int argc, char** argv)
 {
     try{
         {
-            AutomaticDifferentiation::FuncPtr<double,2> x0(new AutomaticDifferentiation::Variable<double,2>(0));
-            AutomaticDifferentiation::FuncPtr<double,2> x1(new AutomaticDifferentiation::Variable<double,2>(1));
-            AutomaticDifferentiation::FuncPtr<double,2> c(new AutomaticDifferentiation::Constant<double,2>(3.0));
-            AutomaticDifferentiation::FuncPtr<double,2> y=x0*x0+x0*x1+x0/x1+c;
+            using namespace AutomaticDifferentiation;
+            FuncPtr<double> x0(new Variable<double>(0));
+            FuncPtr<double> x1(new Variable<double>(1));
+            FuncPtr<double> c(new Constant<double>(3.0));
+            FuncPtr<double> y=x0*x0+x0*x1+x0/x1+c;
 
-#ifdef WITHOUT_EIGEN
-            const std::array<double,2> val{10.0,2.0};
-#else
-            Eigen::Matrix<double,2,1> val{10.0,2.0};
-#endif // WITHOUT_EIGEN
+            const std::vector<double> val{10.0,2.0};
             std::cout << "y(val)=" << (*y)(val) << std::endl;
-            AutomaticDifferentiation::FuncPtr<double,2> dy_0=(*y).derivative(0);
+
+            FuncPtr<double> dy_0=(*y).derivative(0);
             std::cout << "dy_0(val)" << (*dy_0)(val) << std::endl;
-            AutomaticDifferentiation::FuncPtr<double,2> dy_0_1=(*dy_0).derivative(1);
+
+            FuncPtr<double> dy_0_1=(*dy_0).derivative(1);
             std::cout << "dy_0_1(val)" << (*dy_0_1)(val) << std::endl;
         }
         std::cout << std::endl;
         {
-            AutomaticDifferentiation::FuncPtr<double,1> x(new AutomaticDifferentiation::Variable<double,1>(0));
-            AutomaticDifferentiation::FuncPtr<double,1> y=exp(x);
-#ifdef WITHOUT_EIGEN
-            const std::array<double,1> val{M_PI/3.0};
-#else
-            const Eigen::Matrix<double,1,1> val{1.0};
-#endif // WITHOUT_EIGEN
+            using namespace AutomaticDifferentiation_Vector;
+            FuncPtr<double> x(new Variable<double>(0));
+            FuncPtr<double> y=exp(x);
+            const std::vector<double> val{1.0};
             std::cout << "y(val)=" << (*y)(val) << std::endl;
-            AutomaticDifferentiation::FuncPtr<double,1> dy=(*y).derivative();
+            FuncPtr<double> dy=(*y).derivative();
             std::cout << "dy(val)=" << (*dy)(val) << std::endl;
         }
         std::cout << std::endl;
         {
-            auto y=f(AutomaticDifferentiation::createVariables<double,3>());
-            auto jac=AutomaticDifferentiation::jacobian(y);
-            auto hes=AutomaticDifferentiation::hessian(y);
-#ifdef WITHOUT_EIGEN
-            const std::array<double,3> val{10.0, 2.0, 5.0};
-#else
-            const Eigen::Matrix<double,3,1> val{10.0, 2.0, 5.0};
-#endif // WITHOUT_EIGEN
+            using namespace AutomaticDifferentiation_Vector;
+            FuncPtr<double> y=f(createVariables<double>(3));
+            VecFuncPtr<double> jac=jacobian(y,3);
+            MatFuncPtr<double> hes=hessian(y,3);
+            const std::vector<double> val{10.0, 2.0, 5.0};
             std::cout << "y(val)=" << (*y)(val) << std::endl;
-#ifdef WITHOUT_EIGEN
-            std::cout << "jac(y)(val)=" << std::endl << AutomaticDifferentiation::to_string<double,3>(jac(val)) << std::endl;
-            std::cout << "hes(y)(val)=" << std::endl << AutomaticDifferentiation::to_string<double,3>(hes(val)) << std::endl;
-#else
-            std::cout << "jac(y)(val)=" << std::endl << jac(val) << std::endl;
-            std::cout << "hes(y)(val)=" << std::endl << hes(val) << std::endl;
-#endif // WITHOUT_EIGEN
+            std::cout << "jac(y)(val)=" << std::endl << to_string<double>(jac(val)) << std::endl;
+            std::cout << "hes(y)(val)=" << std::endl << to_string<double>(hes(val)) << std::endl;
         }
     }catch(std::string message){
         std::cerr << message << std::endl;
